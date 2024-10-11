990a988d7de3071950ed163acb76627443ce705e
Evgeniy Novikov <isip_e.e.novikov@mpt.ru>
2024-10-11 13:06:52 +0300

    1

---
+++ DiscriminantPercentFinder.Tests/DiscPercFinderTests.cs
@@ -0,0 +1,72 @@
+namespace DiscriminantPercentFinder.Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+[TestClass]
+public class DiscPercFinderTests
+{
+    private static List<(double, double, double, (double?, double?))> testCases;
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext context)
+    {
+        testCases = new List<(double, double, double, (double?, double?))>
+        {
+            (1, 2, 3, (null, null)), // D < 0
+            (1, -2, 1, (1, 1)), // D = 0
+            (1, -3, 2, (2, 1)) // D > 0
+        };
+    }
+
+    [TestMethod]
+    public void TestFindRoots_D_LessThanZero()
+    {
+        var (a, b, c, expected) = testCases[0];
+        var result = DiscPercFinder.FindRoots(a, b, c);
+        var actual = new List<double?> { result.Item1, result.Item2 };
+        var expectedList = new List<double?> { expected.Item1, expected.Item2 };
+        CollectionAssert.AreEqual(expectedList, actual);
+    }
+
+    [TestMethod]
+    public void TestFindRoots_D_EqualToZero()
+    {
+        var (a, b, c, expected) = testCases[1];
+        var result = DiscPercFinder.FindRoots(a, b, c);
+        var actual = new List<double?> { result.Item1, result.Item2 };
+        var expectedList = new List<double?> { expected.Item1, expected.Item2 };
+        CollectionAssert.AreEquivalent(expectedList, actual);
+    }
+
+    [TestMethod]
+    public void TestFindRoots_D_GreaterThanZero()
+    {
+        var (a, b, c, expected) = testCases[2];
+        var result = DiscPercFinder.FindRoots(a, b, c);
+        var actual = new List<double?> { result.Item1, result.Item2 };
+        CollectionAssert.AllItemsAreUnique(actual);
+    }
+    
+    [TestMethod]
+    public void TestCalculatePercentage_Number_ReturnsPercentage()
+    {
+        double number = 200.0;
+        double percentage = 10.0;
+        double expected = 20.0;
+        double result = DiscPercFinder.CalculatePercentage(number, percentage);
+        var actual = new List<double> { result };
+        var expectedList = new List<double> { expected };
+        CollectionAssert.IsSubsetOf(expectedList, actual);
+    }
+    [TestMethod]
+    public void TestCalculatePercentage_Number_ReturnsPercentageWithDelta()
+    {
+        double number = 200.0;
+        double percentage = 10.0;
+        double expected = 20.0;
+        double delta = 0.01;
+        double result = DiscPercFinder.CalculatePercentage(number, percentage);
+        Assert.AreEqual(expected, result, delta);
+    }
+}
---
+++ DiscriminantPercentFinder/DiscPercFinder.cs
@@ -0,0 +1,35 @@
+﻿namespace DiscriminantPercentFinder
+{
+    public class DiscPercFinder
+    {
+        public static (double?, double?) FindRoots(double a, double b, double c)
+        {
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant < 0)
+            {
+                return (null, null);
+            }
+            else if (discriminant == 0)
+            {
+                double root = -b / (2 * a);
+                return (root, root);
+            }
+            else
+            {
+                double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                return (root1, root2);
+            }
+        }
+
+        public static double CalculatePercentage(double number, double percentage)
+        {
+            return (number * percentage) / 100;
+        }
+
+        public static void Main()
+        {
+            
+        }
+    }
+}
